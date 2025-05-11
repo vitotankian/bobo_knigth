068c9a0e1db0bc1209251ecd08de6e58d31f1a85
vitotankian <108244048+vitotankian@users.noreply.github.com>
2025-05-11 19:23:52 +0200

    Update data.js

--- js/data.js
+++ js/data.js
@@ -138,7 +138,7 @@ var autoBuyEnabled = true
 
 const updateSpeed = 20
 const baseLifespan = 365 * 70
-const baseGameSpeed = 4
+const baseGameSpeed = 40
 const heroIncomeMult = 2.5e18
 
 const permanentUnlocks = ["Quick task display", "Evil perks", "Rebirth tab", "Dark Matter", "Dark Matter Skills", "Dark Matter Skills2", "Metaverse", "Metaverse Perks", "Metaverse Perks Button", "Congratulations"]
